fa9a75377e7eb52aeaac064969819f7a29222aa0
Carl Peto <carl.peto@me.com>
2020-09-09 14:13:54 +0100

    Initial Commit

---
+++ NeoUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeoUI
+//
+//  Created by Carl Peto on 09/09/2020.
+//  Copyright © 2020 Carl Peto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
